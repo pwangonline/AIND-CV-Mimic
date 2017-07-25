3c4399da045349f83ca6ed6077a9b3022b2e4e46
Peng Wang <pwang@esri.com>
2017-07-24 23:42:54 -0700

    add feature point and emoji

--- mimic.js
+++ mimic.js
@@ -4,9 +4,10 @@
 // --- Affectiva setup ---
 
 // The affdex SDK Needs to create video and canvas elements in the DOM
-var divRoot = $("#camera")[0];  // div node where we want to add these elements
-var width = 640, height = 480;  // camera image size
-var faceMode = affdex.FaceDetectorMode.LARGE_FACES;  // face mode parameter
+var divRoot = $("#camera")[0]; // div node where we want to add these elements
+var width = 640,
+  height = 480; // camera image size
+var faceMode = affdex.FaceDetectorMode.LARGE_FACES; // face mode parameter
 
 // Initialize an Affectiva CameraDetector object
 var detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
@@ -20,7 +21,7 @@ detector.detectAllAppearance();
 // --- Utility values and functions ---
 
 // Unicode values for all emojis Affectiva can detect
-var emojis = [ 128528, 9786, 128515, 128524, 128527, 128521, 128535, 128539, 128540, 128542, 128545, 128563, 128561 ];
+var emojis = [128528, 9786, 128515, 128524, 128527, 128521, 128535, 128539, 128540, 128542, 128545, 128563, 128561];
 
 // Update target emoji being displayed by supplying a unicode value
 function setTargetEmoji(code) {
@@ -29,7 +30,7 @@ function setTargetEmoji(code) {
 
 // Convert a special character to its unicode value (can be 1 or 2 units long)
 function toUnicode(c) {
-  if(c.length == 1)
+  if (c.length == 1)
     return c.charCodeAt(0);
   return ((((c.charCodeAt(0) - 0xD800) * 0x400) + (c.charCodeAt(1) - 0xDC00) + 0x10000));
 }
@@ -49,8 +50,8 @@ function log(node_name, msg) {
 // Start button
 function onStart() {
   if (detector && !detector.isRunning) {
-    $("#logs").html("");  // clear out previous log
-    detector.start();  // start detector
+    $("#logs").html(""); // clear out previous log
+    detector.start(); // start detector
   }
   log('#logs', "Start button pressed");
 }
@@ -60,7 +61,7 @@ function onStop() {
   log('#logs', "Stop button pressed");
   if (detector && detector.isRunning) {
     detector.removeEventListener();
-    detector.stop();  // stop detector
+    detector.stop(); // stop detector
   }
 };
 
@@ -70,32 +71,32 @@ function onReset() {
   if (detector && detector.isRunning) {
     detector.reset();
   }
-  $('#results').html("");  // clear out results
-  $("#logs").html("");  // clear out previous log
+  $('#results').html(""); // clear out results
+  $("#logs").html(""); // clear out previous log
 
   // TODO(optional): You can restart the game as well
   // <your code here>
 };
 
 // Add a callback to notify when camera access is allowed
-detector.addEventListener("onWebcamConnectSuccess", function() {
+detector.addEventListener("onWebcamConnectSuccess", function () {
   log('#logs', "Webcam access allowed");
 });
 
 // Add a callback to notify when camera access is denied
-detector.addEventListener("onWebcamConnectFailure", function() {
+detector.addEventListener("onWebcamConnectFailure", function () {
   log('#logs', "webcam denied");
   console.log("Webcam access denied");
 });
 
 // Add a callback to notify when detector is stopped
-detector.addEventListener("onStopSuccess", function() {
+detector.addEventListener("onStopSuccess", function () {
   log('#logs', "The detector reports stopped");
   $("#results").html("");
 });
 
 // Add a callback to notify when the detector is initialized and ready for running
-detector.addEventListener("onInitializeSuccess", function() {
+detector.addEventListener("onInitializeSuccess", function () {
   log('#logs', "The detector reports initialized");
   //Display canvas instead of video feed because we want to draw the feature points on it
   $("#face_video_canvas").css("display", "block");
@@ -108,7 +109,7 @@ detector.addEventListener("onInitializeSuccess", function() {
 // Add a callback to receive the results from processing an image
 // NOTE: The faces object contains a list of the faces detected in the image,
 //   probabilities for different expressions, emotions and appearance metrics
-detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
+detector.addEventListener("onImageResultsSuccess", function (faces, image, timestamp) {
   var canvas = $('#face_video_canvas')[0];
   if (!canvas)
     return;
@@ -120,10 +121,10 @@ detector.addEventListener("onImageResultsSuccess", function(faces, image, timest
   if (faces.length > 0) {
     // Report desired metrics
     log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
-    log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
+    log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function (key, val) {
       return val.toFixed ? Number(val.toFixed(0)) : val;
     }));
-    log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
+    log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function (key, val) {
       return val.toFixed ? Number(val.toFixed(0)) : val;
     }));
     log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
@@ -148,14 +149,16 @@ function drawFeaturePoints(canvas, img, face) {
   // TODO: Set the stroke and/or fill style you want for each feature point marker
   // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D#Fill_and_stroke_styles
   // <your code here>
-  
+  ctx.strokeStyle = 'white';
   // Loop over each feature point in the face
   for (var id in face.featurePoints) {
     var featurePoint = face.featurePoints[id];
-
     // TODO: Draw feature point, e.g. as a circle using ctx.arc()
     // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
     // <your code here>
+    ctx.beginPath();
+    ctx.arc(featurePoint.x, featurePoint.y, 2, 0, Math.PI * 2, true);
+    ctx.stroke();
   }
 }
 
@@ -166,11 +169,12 @@ function drawEmoji(canvas, img, face) {
 
   // TODO: Set the font and style you want for the emoji
   // <your code here>
-  
+  ctx.font = '60px Helvetical';
   // TODO: Draw it using ctx.strokeText() or fillText()
   // See: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
   // TIP: Pick a particular feature point as an anchor so that the emoji sticks to your face
   // <your code here>
+  ctx.fillText(face.emojis.dominantEmoji, face.featurePoints[0].x - 50, face.featurePoints[0].y - 50);
 }
 
 // TODO: Define any variables and functions to implement the Mimic Me! game mechanics
